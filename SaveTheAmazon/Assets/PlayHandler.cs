9993d371d8972be161b29d69957a83a5ba77b78e
1612628 <thanhnguyenduy2304@gmail.com>
2019-10-27 23:55:52 +0700

    A03

---
+++ SaveTheAmazon/Assets/PlayHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayHandler : MonoBehaviour
+{
+    public SceneController sceneController;
+    
+    public void playGame()
+    {
+        sceneController.gotoIngameScene1();
+    }
+}
---
+++ SaveTheAmazon/Assets/PlayerHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public SceneController sceneController;
+
+    public void playGame()
+    {
+        sceneController.gotoIngameScene1();
+    }
+}
