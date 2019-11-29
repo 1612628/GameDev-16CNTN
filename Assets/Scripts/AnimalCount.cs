8c51b13e647a0ffb98665aabf02008a6f2043292
1612628 <thanhnguyenduy2304@gmail.com>
2019-11-29 15:40:56 +0700

    semi final 1

---
+++ Assets/Scripts/AnimalCount.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimalCount : MonoBehaviour
+{
+    private int animalCount = 0;
+   
+    public Collider2D checkCollider;
+    public CarryingObject player;
+    public int totalAnimal;
+
+    public TextMeshProUGUI total;
+    public TextMeshProUGUI current;
+
+    public UnityEvent unityEvent;
+
+    private void Start()
+    {
+        if(unityEvent == null)
+        {
+            unityEvent = new UnityEvent();
+        }
+        total.text = totalAnimal.ToString();
+    }
+
+    private void Update()
+    {
+        List<Collider2D> colliders = new List<Collider2D>();
+        checkCollider.OverlapCollider(new ContactFilter2D(), colliders);
+        for (int i = 0; i < colliders.Count; i++)
+        {
+            GameObject beingCarriedObject = player.currentObject ? player.currentObject.gameObject : null;
+            if (colliders[i].CompareTag("Animal") && !Object.Equals(colliders[i].gameObject, beingCarriedObject))
+            {
+                colliders[i].gameObject.SetActive(false);
+                animalCount++;
+                current.text = animalCount.ToString();
+            }
+
+            if(animalCount == totalAnimal)
+            {
+                unityEvent.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioSource backgroundMusic;
+
+    public void SetLevel(float sliderValue)
+    {
+        backgroundMusic.volume = sliderValue;
+    }
+}
